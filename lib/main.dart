e6dd0e302d6ce77a6776ca48ba45bad5c01b80a1
Taylor Gleason <taylor@buzzedtop.com>
2025-04-08 22:22:37 -0400

    Updates to Header

--- lib/main.dart
+++ lib/main.dart
@@ -44,15 +44,18 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          
-          color: Theme.of(context).colorScheme.secondary,
-          padding: const EdgeInsets.all(8.0),
-          child: const Text('Buzzed Top, LLC'),
+    return Container(
+      color: Theme.of(context).colorScheme.secondary,
+      padding: const EdgeInsets.all(8.0),
+      child: DefaultTextStyle(
+        style: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Buzzed Top, LLC'),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
