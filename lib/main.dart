b12a5ff98694d74b6c2b448e5c44f8414a2a8e06
gleatd01 <taylor@buzzedtop.com>
2025-04-11 12:46:54 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/widgets.dart';
-import 'src/header.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,6 +36,28 @@ class HomeScreen extends StatelessWidget {
           Expanded(child: MyHomePage()),
           Footer(),
         ],
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).colorScheme.secondary,
+      padding: const EdgeInsets.all(8.0),
+      child: DefaultTextStyle(
+        style: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(CompanyLogos.buzzedtop, size: 48.0, color: Theme.of(context).colorScheme.onSecondary),
+            const Text('Buzzed Top, LLC'),
+          ],
+        ),
       ),
     );
   }
