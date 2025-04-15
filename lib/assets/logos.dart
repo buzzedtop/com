e340935b121261ec77dce956ffc4c97bb0000d84
gleatd01 <taylor@buzzedtop.com>
2025-04-15 17:02:41 -0400

    Create logos.dart

---
+++ lib/assets/logos.dart
@@ -0,0 +1,9 @@
+
+class CompanyLogos {
+  CompanyLogos._();
+
+  static const _kFontFam = 'CompanyLogos';
+  static const String? _kFontPkg = null;
+
+  static const IconData buzzedtop = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
