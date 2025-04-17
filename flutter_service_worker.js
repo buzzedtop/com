825cc2b142bef40428ba28afb9d17da0af872406
gleatd01 <gleatd01@users.noreply.github.com>
2025-04-17 08:51:13 -0400

    deploy: 28e79c4bdc56c2515b575dec7ad74ee6e732c4cc

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f5e420e0d479558cd2b110d1dbc65c69",
+const RESOURCES = {"flutter_bootstrap.js": "b80778d8be0c1bd6560fe675948fd50b",
 "version.json": "8047e940f128439cae4f54734288a5c4",
 "index.html": "c9604133dce340e125af6092d9a4003d",
 "/": "c9604133dce340e125af6092d9a4003d",
